85d8c92ca137476280b04cf0283c2c27408eb379
tonybierman <tonybierman@gmail.com>
2025-04-09 06:13:06 -0500

    Fine tuning

--- CanineMer/Program.cs
+++ CanineMer/Program.cs
@@ -1,5 +1,4 @@
 using CanineMer;
-using MatBlazor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -8,6 +7,5 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddMatBlazor();
 
 await builder.Build().RunAsync();
